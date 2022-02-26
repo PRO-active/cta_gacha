7308a9ecb573e872adbd7e4991731c6fc57d56fe
banana <hayamatsu8810@gmail.com>
2022-02-27 02:28:25 +0900

    first commit

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias ui-up='docker-compose up sw-ui'
+alias db-up='docker-compose up -d db'
+
+alias ui-bash='docker exec -it sw_ui sh'
---
+++ readme.md
@@ -0,0 +1,11 @@
+# setup
+
+```
+source aliases.sh
+```
+
+# container up
+```
+ui-up # swagger ui
+db-up # mysql container
+```
