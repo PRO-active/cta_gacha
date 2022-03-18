d50acc5f909effdb05f693974e25bf914dc27cc1
banana <hayamatsu8810@gmail.com>
2022-03-19 01:34:48 +0900

    [update] コマンドの修正

--- aliases.sh
+++ aliases.sh
@@ -3,5 +3,5 @@ alias db-up='docker-compose up -d db'
 
 alias ui-bash='docker exec -it sw_ui sh'
 
-alias mi-up='sql-migrate up'
-alias mi-down='sql-migrate down'
+alias migrate-lcl-up='sql-migrate up'
+alias migrate-lcl-down='sql-migrate down'
