0bca8073f003c6373c6f4af2c477f90a031c0b61
banana <hayamatsu8810@gmail.com>
2022-03-18 16:00:16 +0900

    [ADD] migrationコマンドの追加

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,6 @@ alias ui-up='docker-compose up sw-ui'
 alias db-up='docker-compose up -d db'
 
 alias ui-bash='docker exec -it sw_ui sh'
+
+alias mi-up='sql-migrate up'
+alias mi-down='sql-migrate down'
