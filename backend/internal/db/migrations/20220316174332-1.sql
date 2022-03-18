4eadb5b697dba9d35ae11b4675a43eb67305dba1
banana <hayamatsu8810@gmail.com>
2022-03-18 15:15:34 +0900

    [delete] internal内のmigration関連の削除

--- backend/internal/db/migrations/20220316174332-1.sql
+++
@@ -1,5 +0,0 @@
-
--- +migrate Up
-create table if not exists Users (id char primary key, name char, password varchar(10), email char, hash varchar(10));
--- +migrate Down
-drop table if exists Users;
