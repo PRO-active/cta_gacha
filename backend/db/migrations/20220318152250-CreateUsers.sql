c9a4c7af2d895a4877dd2e6ef55b224560159c5d
banana <hayamatsu8810@gmail.com>
2022-03-18 16:01:41 +0900

    [update] カラムごとの改行

--- backend/db/migrations/20220318152250-CreateUsers.sql
+++ backend/db/migrations/20220318152250-CreateUsers.sql
@@ -1,5 +1,11 @@
 
 -- +migrate Up
-create table if not exists Users (id char primary key, name char, password varchar(10), email char, hash varchar(10));
+create table if not exists Users (
+  id char primary key,
+  name char,
+  password varchar(10),
+  email char,
+  hash varchar(10)
+  );
 -- +migrate Down
 drop table if exists Users;
