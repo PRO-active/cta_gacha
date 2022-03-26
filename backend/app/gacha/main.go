04dc2174b3df17d55e6d21834aae840a9d92a0c5
banana <hayamatsu8810@gmail.com>
2022-03-26 22:23:12 +0900

    [fix] dockerfile

--- backend/app/gacha/main.go
+++ backend/app/gacha/main.go
@@ -11,7 +11,8 @@ import (
 
 func main() {
 
-	dsn := "root:password@tcp(127.0.0.1:16002)/tutorial?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:password@tcp(127.0.0.1:16002)/tutorial?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password@tcp(db:3306)/tutorial?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +33,7 @@ func main() {
 	server := api.InitalizeServer(db)
 	// server.Server.ListenAndServe()
 	// firewallが出てくるのでローカルでホストを持たせる
-	if err := server.Start("127.0.0.1:8080"); err != http.ErrServerClosed {
+	if err := server.Start("0.0.0.0:8080"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
 }
